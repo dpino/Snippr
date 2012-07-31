b63b6effbda4dd198143a45b3d60a66c12ac9ad1
Miguel Gonzalez <migonzalvar@gmail.com>
2012-08-01 00:29:18 +0200

    Create Comment entity
    It has the following fields:
    
    - text. String
    
    - email. String. Email for the user. This will be used for anonymous
    	comments.
    
    - url. String. URL for a web the user is associated with. This
    	will be used for anonymous comments.

---
+++ src/main/java/org/snippr/business/entities/Comment.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of Snippr
+ *
+ * Copyright (C) 2012 Igalia, S.L.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.snippr.business.entities;
+
+
+/**
+ * @author Miguel Gonzalez <migonzalvar@gmail.com>
+ */
+public class Comment extends BaseEntity {
+
+    private String text;
+    private String email;
+    private String url;
+
+
+    public Comment() {
+        super();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
