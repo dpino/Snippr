010ef9e4edd01dfc8f3557e929bef512e29e4c08
Diego Bernardez <diego.bernardez.moron@gmail.com>
2012-08-13 18:13:56 +0200

    Added the Hibernate support for Comments
    Now the ICommentDAO interface and the CommentDAO class extend the GenericDAOHibernate interface in order to give the Hibernate support.
    Also the Snippr.hbm.xml file has been edited in order to map the Comment class with the Comment table into the Postgres database manager.

--- src/main/java/org/snippr/business/dao/CommentDAO.java
+++ src/main/java/org/snippr/business/dao/CommentDAO.java
@@ -1,14 +1,25 @@
 package org.snippr.business.dao;
 
 import java.util.List;
-
 import org.snippr.business.entities.Comment;
 
-public class CommentDAO implements ICommentDAO{
+/**
+ *
+ * @author Diego Bernardez
+ *
+ */
+
+public class CommentDAO extends GenericDAOHibernate<Comment, Long> implements ICommentDAO{
 
 	@Override
 	public List<Comment> getAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean exists(Comment comment) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- src/main/java/org/snippr/business/dao/ICommentDAO.java
+++ src/main/java/org/snippr/business/dao/ICommentDAO.java
@@ -1,10 +1,16 @@
 package org.snippr.business.dao;
 
 import java.util.List;
-
 import org.snippr.business.entities.Comment;
 
-public interface ICommentDAO {
+/**
+ *
+ * @author Diego Bernardez
+ *
+ */
+
+public interface ICommentDAO extends IGenericDAO<Comment, Long>{
 
 	List <Comment> getAll();
+	boolean exists(Comment comment);
 }
